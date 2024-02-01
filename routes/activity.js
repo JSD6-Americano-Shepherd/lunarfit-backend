08af90192a3d45b9c10bef5e85c8aac7e149205f
bluebirds2920 <110462756+bluebirds2920@users.noreply.github.com>
2024-02-01 23:31:57 +0700

    commit

--- routes/activity.js
+++ routes/activity.js
@@ -1,7 +1,35 @@
 import express from "express";
 const activityRouter = express.Router();
 
+
 activityRouter.get("/", (req, res) => res.send("This is routeractivity "));
+
+
+
+
+activityRouter.post("/", async (req, res) => {
+    let body = req.body;
+    const [isBodyChecked, missingFields] = checkMissingField(
+        HEALTH_DATA_KEYS,
+        body
+    );
+    if (!isBodyChecked) {
+        res.send(`Missing Fields: ${"".concat(missingFields)}`);
+        return;
+    }
+
+    body["user_id"] = new ObjectId(body.user_id);
+
+    await databaseClient.db().collection("health-history").insertOne(body);
+    res.send("Create health data successfully");
+});
+
+
+
+
+
+
+
 
 
 
