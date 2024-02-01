ae16cdfc3510a00a3ebe9497294bd10042c9c584
Upond <pond_chanawin@hotmail.com>
2024-02-01 23:19:37 +0700

    Update: Update activity and signup that receive data request from frontend

--- routes/activity.js
+++ routes/activity.js
@@ -1,9 +1,33 @@
 import express from "express";
+import databaseClient from "../configs/database.mjs";
+import { checkMissingField } from "../utils/requestUtils.js";
 const activityRouter = express.Router();
-
+const ACTIVITY_DATA_KEYS = [
+  "userId",
+  "type",
+  "name",
+  "date",
+  "start",
+  "end",
+  "note",
+  "image",
+];
 activityRouter.get("/", (req, res) => res.send("This is routeractivity "));
 
+activityRouter.post("/", async (req, res) => {
+  let activity = req.body;
+  const [isBodyChecked, missingFields] = checkMissingField(
+    ACTIVITY_DATA_KEYS,
+    activity
+  );
 
+  if (!isBodyChecked) {
+    res.send(`Missing Fields: ${"".concat(missingFields)}`);
+    return;
+  }
+
+  await databaseClient.db().collection("activities").insertOne(activity);
+  res.send("Create activity data successfully");
+});
 
 export default activityRouter;
-
--- routes/signup.js
+++ routes/signup.js
@@ -1,8 +1,37 @@
 import express from "express";
+import databaseClient from "../configs/database.mjs";
+import { ObjectId } from "mongodb";
+import { checkMissingField } from "../utils/requestUtils.js";
 const signUpRouter = express.Router();
+const USER_DATA_KEYS = ["firstName", "lastName", "email", "password"];
 
 signUpRouter.get("/", (req, res) => res.send("This is signUpRouter "));
 
+signUpRouter.post("/", async (req, res) => {
+  let user = req.body;
+  const [isBodyChecked, missingFields] = checkMissingField(
+    USER_DATA_KEYS,
+    user
+  );
+  if (!user.firstName) {
+    res.status(400).send("Firstname cannot empty");
+    return;
+  }
+  if (!isBodyChecked) {
+    res.status(404).send(`Missing Fields: ${"".concat(missingFields)}`);
+    return;
+  }
 
+  const data = {
+    ...user,
+    weight: 0,
+    height: 0,
+    age: 0,
+    gender: "",
+  };
+  await databaseClient.db().collection("users").insertOne(data);
+  res.status(200).send("Create user data successfully");
+  //   res.redirect("/home");
+});
 
 export default signUpRouter;
