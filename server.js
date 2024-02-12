81af4fa5a18e2be3e6fa58a343f2d23c030e905e
bluebirds2920 <110462756+bluebirds2920@users.noreply.github.com>
2024-02-12 19:22:57 +0700

    recors

--- server.js
+++ server.js
@@ -29,25 +29,6 @@ const webServer = express();
 // };
 
 
-
-
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (allowedOrigins.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-//   credentials: true, // อนุญาตให้ส่ง cookies และ headers ระหว่างโดเมน
-// };
-
-
-
-
-
-
-
 // ใช้ cors middleware โดยใช้ options ที่กำหนด
 //webServer.use(cors(corsOptions));
 webServer.use(cors({
