7dcbec51748828ffcbb1e4786857148730524bda
Upond <pond_chanawin@hotmail.com>
2024-02-16 14:45:08 +0700

    Update: account

--- routes/account.js
+++ routes/account.js
@@ -3,31 +3,41 @@ import databaseClient from "../configs/database.mjs";
 import { checkMissingField } from "../utils/requestUtils.js";
 import authenticateToken from "../middlewares/authenticatetoken.js";
 import { ObjectId } from "mongodb";
+import uploadToCloudinary from "../middlewares/uploadToCloudinary.js";
 const accountRouter = express.Router();
 
 accountRouter.patch("/", authenticateToken, async (req, res) => {
   const { email } = req.data.user;
   const updateData = req.body;
+  const { profileimg } = req.body;
+
   try {
-    // Convert activityId from string to ObjectId for the query
-    const result = await databaseClient.db().collection("users").updateOne(
-      { email }, // Ensure the activity belongs to the user
-      { $set: updateData } // Use $set operator to update the fields
-    );
+    if (profileimg) {
+      if (profileimg !== "") {
+        const cloudinaryResult = await uploadToCloudinary(profileimg);
+        const imageUrl = cloudinaryResult.secure_url;
+        updateData.profileimg = imageUrl;
+      } else {
+        updateData.profileimg = "";
+      }
+    }
+
+    const result = await databaseClient
+      .db()
+      .collection("users")
+      .updateOne({ email }, { $set: updateData });
 
     if (result.matchedCount === 0) {
-      // No document found to update
       res.status(404).send("You do not have permission to update it.");
     } else if (result.modifiedCount === 0) {
-      // Document found but no changes made (possibly due to the update data being the same as the existing data)
       res.status(200).send("No changes were made to the user.");
     } else {
-      // Document updated successfully
       res.status(200).send("User updated successfully.");
     }
   } catch (error) {
     console.error(error);
-    res.status(500).send("An error occurred while updating the activity.");
+    res.status(500).send("An error occurred while updating the user.");
   }
 });
+
 export default accountRouter;
