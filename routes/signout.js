553ecb24f9d42eedb59ef5a1a6bb8be39e8d6eb1
Upond <pond_chanawin@hotmail.com>
2024-02-16 23:58:18 +0700

    update signout

--- routes/signout.js
+++ routes/signout.js
@@ -1,12 +1,19 @@
-import express from 'express';
-
+import express from "express";
+import { createJwt } from "../utils/jwtUtils.js";
+import authenticateToken from "../middlewares/authenticatetoken.js";
 const signOutRouter = express.Router();
 
-signOutRouter.get('/', (req, res) => {
+signOutRouter.get("/", authenticateToken, (req, res) => {
+  const newToken = createJwt(null); // สร้าง token ใหม่ด้วยอีเมลใหม่
+  const tenDaysInMilliseconds = 10 * 24 * 60 * 60 * 1000;
+  res.cookie("token", newToken, {
+    maxAge: tenDaysInMilliseconds,
+    secure: true,
+    httpOnly: true,
+    sameSite: "none",
+  });
 
-    res.clearCookie("token");
-
-    res.status(200).send('Logged out successfully');
+  res.status(200).send("Logged out successfully");
 });
 
 export default signOutRouter;
