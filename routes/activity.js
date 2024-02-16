f67069cca8fa97c9d8f199285b124ab156122a69
Upond <pond_chanawin@hotmail.com>
2024-02-16 10:37:36 +0700

    Update

--- routes/activity.js
+++ routes/activity.js
@@ -5,6 +5,7 @@ import authenticateToken from "../middlewares/authenticatetoken.js";
 import { ObjectId } from "mongodb";
 import { calculateDuration } from "../utils/calDuration.js";
 import uploadToCloudinary from "../middlewares/uploadToCloudinary.js";
+
 const activityRouter = express.Router();
 const ACTIVITY_DATA_KEYS = [
   "type",
@@ -31,50 +32,62 @@ activityRouter.get("/", authenticateToken, async (req, res) => {
   const activityData = await databaseClient
     .db()
     .collection("activities")
-    .find({ userId: new ObjectId(userId) }) // Add a query filter to select documents where userId is "01"
+    .find({ userId: new ObjectId(userId) })
     .toArray();
   res.json(activityData);
 });
 
 activityRouter.post("/", authenticateToken, async (req, res) => {
-  const { email } = req.data.user;
-  const { image } = req.body;
-  let activity = req.body;
+  try {
+    const { email } = req.data.user;
+    const { image } = req.body;
+    let activity = req.body;
 
-  const userData = await databaseClient
-    .db()
-    .collection("users")
-    .findOne({ email });
+    const userData = await databaseClient
+      .db()
+      .collection("users")
+      .findOne({ email });
 
-  const userId = userData._id;
-  const duration = calculateDuration(activity.start, activity.end).toString();
+    const userId = userData._id;
+    const duration = calculateDuration(activity.start, activity.end).toString();
 
-  const [year, month, day] = activity.date.split("-").map(Number);
-  console.log("Duration: ", duration);
-  console.log(year, month, day);
+    const [year, month, day] = activity.date.split("-").map(Number);
+    console.log("Duration: ", duration);
+    console.log(year, month, day);
 
-  const sumActivity = {
-    ...activity,
-    duration: duration,
-    year: year.toString(),
-    month: month.toString(),
-    day: day.toString(),
-    userId: new ObjectId(userId),
-  };
-  const [isBodyChecked, missingFields] = checkMissingField(
-    ACTIVITY_DATA_KEYS,
-    sumActivity
-  );
+    const sumActivity = {
+      ...activity,
+      duration: duration,
+      year: year.toString(),
+      month: month.toString(),
+      day: day.toString(),
+      userId: new ObjectId(userId),
+    };
+    const [isBodyChecked, missingFields] = checkMissingField(
+      ACTIVITY_DATA_KEYS,
+      sumActivity
+    );
 
-  if (!isBodyChecked) {
-    res.send(`Missing Fields: ${"".concat(missingFields)}`);
-    return;
+    if (!isBodyChecked) {
+      res.send(`Missing Fields: ${"".concat(missingFields)}`);
+      return;
+    }
+
+    try {
+      const cloudinaryResult = await uploadToCloudinary(image);
+      const imageUrl = cloudinaryResult.secure_url;
+      sumActivity.image = imageUrl;
+      await databaseClient.db().collection("activities").insertOne(sumActivity);
+      res.send("Create activity data successfully");
+    } catch (error) {
+      console.error("Error uploading image to Cloudinary:", error);
+      res.status(500).json({ message: "Error uploading image to Cloudinary" });
+      throw error;
+    }
+  } catch (error) {
+    console.error("Error creating activity:", error);
+    res.status(500).json({ message: "Error creating activity" });
   }
-  const cloudinaryResult = await uploadToCloudinary(image);
-  const imageUrl = cloudinaryResult.secure_url;
-  sumActivity.image = imageUrl;
-  await databaseClient.db().collection("activities").insertOne(sumActivity);
-  res.send("Create activity data successfully");
 });
 
 activityRouter.delete("/:id", async (req, res) => {
